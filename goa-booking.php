d7a33d504e15057018daf13eb2c84bc1d5fe512f
Sergio Hardov <sergiohardov@gmail.com>
2023-03-20 01:07:01 +0200

    Git init

---
+++ goa-booking.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name: Games of Arts Booking
+ * Description: Система бронирования, и учета клиентов для Games of Arts.
+ * Author URI:  https://t.me/sergiohardov
+ * Author:      Sergio Hardov
+ * Version:     1.0
+ * Text Domain: arts-booking
+ * Domain Path: /lang
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
