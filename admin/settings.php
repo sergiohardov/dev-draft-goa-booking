64617d650bcc081005828102d2b718629fe93f70
Sergio <sergiohardov@gmail.com>
2023-03-20 15:05:52 +0200

    Черновая верстка страницы настроек, выведены заглушки таблицы агентов и клиентов

--- admin/settings.php
+++ admin/settings.php
@@ -1,21 +1,118 @@
-<div class="card text-center">
-    <div class="card-header">
-        <ul class="nav nav-tabs card-header-tabs">
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="true" href="#">Active</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled">Disabled</a>
-            </li>
-        </ul>
-    </div>
-    <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
+<div class="goa-booking-plugin-settings">
+    <div class="container-fluid pt-3 pb-3">
+        <div class="row">
+            <h1 class="text-center"><?php echo get_admin_page_title(); ?></h1>
+        </div>
 
-    
+        <div class="row">
+            <h2 class="text-center">Agents</h2>
+            <table class="table table-striped table-sm align-middle">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">First Name</th>
+                        <th scope="col">Last Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Login</th>
+                        <th scope="col">Bio</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>First Example</td>
+                        <td>Last Example</td>
+                        <td>example@mdo</td>
+                        <td>+380933333333</td>
+                        <td>examplelogin</td>
+                        <td style="width: 450px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod impedit saepe vero illo cupiditate exercitationem inventore, autem iure dolore sapiente!</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>First Example</td>
+                        <td>Last Example</td>
+                        <td>example@mdo</td>
+                        <td>+380933333333</td>
+                        <td>examplelogin</td>
+                        <td style="width: 450px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod impedit saepe vero illo cupiditate exercitationem inventore, autem iure dolore sapiente!</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>First Example</td>
+                        <td>Last Example</td>
+                        <td>example@mdo</td>
+                        <td>+380933333333</td>
+                        <td>examplelogin</td>
+                        <td style="width: 450px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod impedit saepe vero illo cupiditate exercitationem inventore, autem iure dolore sapiente!</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>First Example</td>
+                        <td>Last Example</td>
+                        <td>example@mdo</td>
+                        <td>+380933333333</td>
+                        <td>examplelogin</td>
+                        <td style="width: 450px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod impedit saepe vero illo cupiditate exercitationem inventore, autem iure dolore sapiente!</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="row">
+            <h2 class="text-center">Clients</h2>
+            <table class="table table-striped table-sm align-middle">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">First Name</th>
+                        <th scope="col">Last Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Login</th>
+                        <th scope="col">Bio</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>First Example</td>
+                        <td>Last Example</td>
+                        <td>example@mdo</td>
+                        <td>+380933333333</td>
+                        <td>examplelogin</td>
+                        <td style="width: 450px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod impedit saepe vero illo cupiditate exercitationem inventore, autem iure dolore sapiente!</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>First Example</td>
+                        <td>Last Example</td>
+                        <td>example@mdo</td>
+                        <td>+380933333333</td>
+                        <td>examplelogin</td>
+                        <td style="width: 450px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod impedit saepe vero illo cupiditate exercitationem inventore, autem iure dolore sapiente!</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>First Example</td>
+                        <td>Last Example</td>
+                        <td>example@mdo</td>
+                        <td>+380933333333</td>
+                        <td>examplelogin</td>
+                        <td style="width: 450px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod impedit saepe vero illo cupiditate exercitationem inventore, autem iure dolore sapiente!</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>First Example</td>
+                        <td>Last Example</td>
+                        <td>example@mdo</td>
+                        <td>+380933333333</td>
+                        <td>examplelogin</td>
+                        <td style="width: 450px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod impedit saepe vero illo cupiditate exercitationem inventore, autem iure dolore sapiente!</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+</div>
